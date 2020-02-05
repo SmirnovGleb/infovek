0b49b252a5a9aa7fe13e089603c6b4d39296e187
glebsmirnov <glebsmirnov@192.168.100.8>
2020-02-05 23:13:07 +0300

    try

--- infovek/src/main/java/net/infovek/lambda/infoveklamda/LambdaFunctionHandler.java
+++ infovek/src/main/java/net/infovek/lambda/infoveklamda/LambdaFunctionHandler.java
@@ -68,7 +68,7 @@ public class LambdaFunctionHandler implements RequestStreamHandler {
 			responseBody.put("message", "New item created");
 
 			JSONObject headerJson = new JSONObject();
-			headerJson.put("x-custom-header", "11111!!!111");
+			headerJson.put("x-custom-header", "check chek");
 
 			responseJson.put("statusCode", 200);
 			responseJson.put("headers", headerJson);
